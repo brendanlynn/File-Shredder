f6a3e3478c37481a3e2ada30ad6e2f5e8f1730e3
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-06-13 15:12:52 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using static System.Console;
+Title = "File Shredder";
+while (true)
+{
+    string? path = ReadLine();
+    if (path is null)
+    {
+        WriteLine("Path may not be null.");
+        continue;
+    }
+    Stream stream = null!;
+    try
+    {
+        stream = new FileStream(path, FileMode.Open, FileAccess.Write);
+        WriteLine("File accessed. Shredding...");
+        while (stream.Position < stream.Length)
+        {
+            stream.WriteByte(0);
+        }
+
+        WriteLine("File successfully shredded.");
+    }
+    catch (DirectoryNotFoundException)
+    {
+        WriteLine("ERROR: Directory not found.");
+        WriteLine();
+        continue;
+    }
+    catch (FileNotFoundException)
+    {
+        WriteLine("ERROR: File not found.");
+        WriteLine();
+        continue;
+    }
+    catch (UnauthorizedAccessException)
+    {
+        WriteLine("ERROR: Access denied.");
+        WriteLine();
+        continue;
+    }
+    catch (PathTooLongException)
+    {
+        WriteLine("ERROR: Path too long.");
+        WriteLine();
+        continue;
+    }
+    catch (IOException)
+    {
+        WriteLine("ERROR: An unrecognized I/O error occured.");
+        WriteLine();
+        continue;
+    }
+    catch
+    {
+        WriteLine("ERROR: An unrecognized error occured.");
+        WriteLine();
+        continue;
+    }
+    finally
+    {
+        stream?.Dispose();
+    }
+    WriteLine("Deleting file...");
+    try
+    {
+        File.Delete(path);
+        WriteLine("File deleted.");
+    }
+    catch (DirectoryNotFoundException)
+    {
+        WriteLine("ERROR: Directory not found.");
+        WriteLine();
+        continue;
+    }
+    catch (FileNotFoundException)
+    {
+        WriteLine("ERROR: File not found.");
+        WriteLine();
+        continue;
+    }
+    catch (UnauthorizedAccessException)
+    {
+        WriteLine("ERROR: Access denied.");
+        WriteLine();
+        continue;
+    }
+    catch (PathTooLongException)
+    {
+        WriteLine("ERROR: Path too long.");
+        WriteLine();
+        continue;
+    }
+    catch (IOException)
+    {
+        WriteLine("ERROR: An unrecognized I/O error occured.");
+        WriteLine();
+        continue;
+    }
+    catch
+    {
+        WriteLine("ERROR: An unrecognized error occured.");
+        WriteLine();
+        continue;
+    }
+    WriteLine();
+}
